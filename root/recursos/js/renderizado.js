a49a5071f282c7ca9dbda7f922b2e6ce60820806
Scarone Nahuel <nahuelscarone@gmail.com>
2025-12-30 21:07:55 -0300

    feat: agregar en la función de renderizado la etiqueta h2 con la clase tarjeta-producto__bodega

--- root/recursos/js/renderizado.js
+++ root/recursos/js/renderizado.js
@@ -9,7 +9,7 @@ function renderizarCatalogo(arreglo, $contenedor) {
 
                 <div class="tarjeta-producto__informacion">
                     <h2 class="tarjeta-producto__titulo">${element.nombre}</h2>
-
+                    <h2 class="tarjeta-producto__bodega">${element.bodega}</h2>
                     <p class="tarjeta-producto__parrafo">${element.descripcion}</p>
 
                     <p class="tarjeta-producto__precio"><strong>$${element.precio}</strong></p>
